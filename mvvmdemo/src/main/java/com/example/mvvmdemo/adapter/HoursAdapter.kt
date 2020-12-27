4affd674e22742fa14687862c1df8736d78f2a6a
istar_yang <akuistary@gmail.com>
2020-12-27 20:46:47 +0800

    adapterbinditesting

---
+++ mvvmdemo/src/main/java/com/example/mvvmdemo/adapter/HoursAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.mvvmdemo.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mvvmdemo.databinding.HoursListItemsBinding
+
+class HoursAdapter(private val hourList: List<String>) :
+    RecyclerView.Adapter<HoursAdapter.HoursViewHolder> {
+
+
+    inner class HoursViewHolder(val binding: HoursListItemsBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HoursViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: HoursViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
