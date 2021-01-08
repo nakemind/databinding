d48795ce23dc480f1e11a030af27ba534980ef04
istar_yang <akuistary@gmail.com>
2021-01-09 01:27:42 +0800

    测试数据的内容，根据当前的list进行裁剪，但是子数据的list还是不会变化的，没有任何的印象，不会印象sublist后的数据内容

--- mvvmdemo/src/main/java/com/example/mvvmdemo/fragment/MainActivityFragment.kt
+++ mvvmdemo/src/main/java/com/example/mvvmdemo/fragment/MainActivityFragment.kt
@@ -1,6 +1,8 @@
 package com.example.mvvmdemo.fragment
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mvvmdemo.databinding.ActivityMainInflatorBinding
 
@@ -13,12 +15,24 @@ class MainActivityFragment : AppCompatActivity() {
 
         binding=ActivityMainInflatorBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val my = ArrayList<String>();
+        for(i:Int in 1..30) {
+            my.add(i.toString())
+        }
+        Handler(Looper.myLooper()!!).postDelayed(Runnable { my.removeAt(0) },1000)
+
+        val sub=my.subList(0,3)
+        Handler(Looper.myLooper()!!).postDelayed(Runnable { sub.toString() },3000)
+
     }
 
     override fun onStart() {
         super.onStart()
         binding.textViewActivity.text="you are the best people"
     }
+
+
 
 
 }
